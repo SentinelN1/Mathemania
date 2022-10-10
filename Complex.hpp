a634643566947621667148c2c3661b69ea9f6f26
SentinelN1 <egor.a.antonov@gmail.com>
2022-10-10 03:37:02 +0300

    Some more minor tweaks

--- Complex.hpp
+++ Complex.hpp
@@ -7,12 +7,10 @@
 
 class Complex
 {
-public:
-    double real;
-    double imaginary;
-    double modulus;
-    double argument;
+private:
+    double real, imaginary;
 
+public:
     // Constructor
     Complex(const double x = 0, const double y = 0, char form[] = "algebraic")
     {
@@ -23,27 +21,37 @@ public:
         }
         else if (form == "polar")
         {
-            modulus = x;
-            argument = y;
+            // modulus = x;
+            // argument = y;
             real = x * cos(y);
             imaginary = y * sin(y);
         }
     }
 
+    double Re() const
+    {
+        return real;
+    }
+
+    double Im() const
+    {
+        return imaginary;
+    }
+
     // Equality
-    bool operator==(const Complex other) const
+    bool operator==(const Complex &other) const
     {
         return (real == other.real && imaginary == other.imaginary);
     }
 
-    // Unequality
-    bool operator!=(const Complex other) const
+    // Inequality
+    bool operator!=(const Complex &other) const
     {
-        // return (x+yi != a+bi);
+        // return !(this == other);
         return (real != other.real || imaginary != other.imaginary);
     }
 
-    friend std::ostream &operator<<(std::ostream &os, const Complex complex)
+    friend std::ostream &operator<<(std::ostream &os, const Complex &complex)
     {
         os << complex.real << " + " << complex.imaginary << "i";
         return os;
@@ -67,18 +75,17 @@ public:
         return Complex(real, imaginary);
     }
 
-    // Binary plus -- Addition
-    friend Complex operator+(const Complex a, const Complex b)
+    // Addition
+    friend Complex operator+(const Complex &a, const Complex &b)
     {
         return Complex(a.real + b.real, a.imaginary + b.imaginary);
     }
 
     // Increment
-    Complex operator+=(const Complex complex)
+    void operator+=(const Complex &other)
     {
-        real += complex.real;
-        imaginary += complex.imaginary;
-        return Complex(real, imaginary);
+        this->real += other.real;
+        this->imaginary += other.imaginary;
     }
 
     // Unary Minus
@@ -87,31 +94,29 @@ public:
         return Complex(-real, -imaginary);
     }
 
-    // Binary Minus -- Subtraction
-    friend Complex operator-(const Complex a, const Complex b)
+    // Subtraction
+    friend Complex operator-(const Complex &a, const Complex &b)
     {
         return Complex(a.real - b.real, a.imaginary - b.imaginary);
     }
 
     // Decrement
-    Complex operator-=(const Complex complex)
+    void operator-=(const Complex &other)
     {
-        real -= complex.real;
-        imaginary -= complex.imaginary;
-        return Complex(real, imaginary);
+        this->real -= other.real;
+        this->imaginary -= other.imaginary;
     }
 
     // Multiplication
-    friend Complex operator*(const Complex a, const Complex b)
+    friend Complex operator*(const Complex &a, const Complex &b)
     {
         return Complex(a.real * b.real - a.imaginary * b.imaginary, a.real * b.imaginary + a.imaginary * b.real);
     }
 
-    Complex operator*=(const Complex complex)
+    void operator*=(const Complex &other)
     {
-        real = real * complex.real - imaginary * complex.imaginary;
-        imaginary = real * complex.imaginary + imaginary * complex.real;
-        return Complex(real, imaginary);
+        this->real = real * other.real - imaginary * other.imaginary;
+        this->imaginary = real * other.imaginary + imaginary * other.real;
     }
 
     // Complex Conjugate (x - yi)
@@ -120,49 +125,63 @@ public:
         return Complex(real, -imaginary);
     }
 
-    //
+    // Complex reciprocal (Conj(z) / Abs(z)^2)
     Complex Reciprocal() const
     {
         double abs2 = real * real + imaginary * imaginary;
-        if (abs2 != 0)
+
+        if (abs2 == 0)
         {
-            return Complex(real / abs2, -imaginary / abs2);
+            throw "Division by zero";
         }
-        std::cout << "Division by zero ecountered.\n";
-        exit(0);
+
+        return Complex(real / abs2, -imaginary / abs2);
     }
 
-    friend Complex operator/(const Complex a, const Complex b)
+    friend Complex operator/(const Complex &a, const Complex &b)
     {
         return a * b.Reciprocal();
     }
 };
 
-#define M_I Complex(0, 1)
-
-double Re(const Complex complex)
+double Re(const Complex &complex)
 {
-    return complex.real;
+    return complex.Re();
 }
 
-double Im(const Complex complex)
+double Im(const Complex &complex)
 {
-    return complex.imaginary;
+    return complex.Im();
 }
 
-double Abs(const Complex complex)
+double Abs(const Complex &complex)
 {
     return complex.Abs();
 }
 
-double Arg(const Complex complex)
+double Arg(const Complex &complex)
 {
     return complex.Arg();
 }
 
-Complex Log(const Complex complex, const Complex base = M_E)
+Complex Conjugate(const Complex &complex)
 {
-    if (Abs(complex) != 0)
+    return complex.Conjugate();
+}
+
+Complex Reciprocal(const Complex &complex)
+{
+    return complex.Reciprocal();
+}
+
+// Complex logarithm, by default logarithm base is set to e = 2.7182818284590452354
+Complex Log(const Complex &complex, const Complex &base = M_E)
+{
+    if (complex == 0)
+    {
+        throw "Logarithm of 0 is undefined";
+    }
+
     {
         if (base == M_E)
         {
@@ -171,16 +190,14 @@ Complex Log(const Complex complex, const Complex base = M_E)
 
         return Log(complex) / Log(base);
     }
-    std::cout << "Log(0) is undefined.\n";
-    exit(0);
 }
 
-Complex Exp(const Complex complex)
+Complex Exp(const Complex &complex)
 {
-    return exp(complex.real) * Complex(1, complex.imaginary, "polar");
+    return exp(Re(complex)) * Complex(1, Im(complex), "polar");
 }
 
-Complex Pow(const Complex complex, const Complex power)
+Complex Pow(const Complex &complex, const Complex &power)
 {
     double abs = Abs(complex);
     double arg = Arg(complex);
@@ -192,12 +209,12 @@ Complex Pow(const Complex complex, const Complex power)
     return Exp(Log(complex) * power);
 }
 
-Complex Sin(const Complex complex)
+Complex Sin(const Complex &complex)
 {
-    return Complex(sin(complex.real) * cosh(complex.imaginary), cos(complex.real) * sinh(complex.imaginary));
+    return Complex(sin(complex.Re()) * cosh(complex.Im()), cos(complex.Re()) * sinh(complex.Im()));
 }
 
-Complex Cos(const Complex complex)
+Complex Cos(const Complex &complex)
 {
-    return Complex(cos(complex.real) * cosh(complex.imaginary), -sin(complex.real) * sinh(complex.imaginary));
+    return Complex(cos(complex.Re()) * cosh(complex.Im()), -sin(complex.Re()) * sinh(complex.Im()));
 }
