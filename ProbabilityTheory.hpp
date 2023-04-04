c3beaa4084c9171a89b7c3181fd19f35ec8ed0d7
SentinelN1 <egor.a.antonov@gmail.com>
2023-04-05 02:53:42 +0300

    Discrete Random Variable class Expected value, Variance methods

--- ProbabilityTheory.hpp
+++ ProbabilityTheory.hpp
@@ -1,23 +1,86 @@
 #pragma once
+
+#include <vector>
 #include <list>
+#include <set>
+#include <map>
 
-double ExpectedValue(const std::list<std::pair<double, double>> &randomVariable)
+template <typename T>
+class DiscreteRandomVariable
 {
-    double expectedValue = 0;
-    for (auto pair : randomVariable)
-    {
-        expectedValue += pair.first * pair.second;
-    }
-    return expectedValue;
-}
+private:
+    std::map<T, double> probabilities;
 
-double Variance(const std::list<std::pair<double, double>> &randomVariable)
-{
-    double expectedValue = ExpectedValue(randomVariable);
-    double variance = 0;
-    for (auto pair : randomVariable)
+public:
+    void Probabilities() const
     {
-        variance += pair.first * pair.first * pair.second;
+        for (auto i : probabilities)
+        {
+            std::cout << i.first << " : " << i.second << "\n";
+        }
     }
-    return variance - expectedValue * expectedValue;
-}
+
+    DiscreteRandomVariable() = default;
+
+    DiscreteRandomVariable(const std::vector<T> &prob)
+    {
+        for (auto i : prob)
+        {
+            probabilities[i] = 1.0 / prob.size();
+        }
+    }
+
+    DiscreteRandomVariable(const std::list<T> &prob)
+    {
+        for (auto i : prob)
+        {
+            probabilities[i] = 1.0 / prob.size();
+        }
+    }
+
+    DiscreteRandomVariable(const std::set<T> &prob)
+    {
+        for (auto i : prob)
+        {
+            probabilities[i] = 1.0 / prob.size;
+        }
+    }
+
+    DiscreteRandomVariable(const std::map<T, double> &prob)
+    {
+        probabilities = prob;
+    }
+
+    double ExpectedValue() const
+    {
+        double expectedValue = 0;
+        for (auto i : probabilities)
+        {
+            expectedValue += i.first * i.second;
+        }
+        return expectedValue;
+    }
+
+    double Variance() const
+    {
+        double expetedValue = ExpectedValue();
+        double variance = 0;
+        for (auto i : probabilities)
+        {
+            variance += i.first * i.first * i.second;
+        }
+        return variance - expetedValue * expetedValue;
+    }
+
+    DiscreteRandomVariable operator+(const DiscreteRandomVariable &other) const
+    {
+        DiscreteRandomVariable result;
+        for (auto i : probabilities)
+        {
+            for (auto j : other.probabilities)
+            {
+                result.probabilities[i.first + j.first] += i.second * j.second;
+            }
+        }
+    }
+};
--- main.cpp
+++ main.cpp
@@ -1,31 +1,10 @@
 #include <iostream>
-#include <list>
-// #include "Function.hpp"
+#include <vector>
 #include "ProbabilityTheory.hpp"
-
-using std::cin;
-using std::cout;
-using std::endl;
-// using namespace complex;
-// using namespace function;
 
 int main()
 {
-    // std::list<std::pair<double, double>> dice = {{1, 1.0 / 6}, {2, 1.0 / 6}, {3, 1.0 / 6}, {4, 1.0 / 6}, {5, 1.0 / 6}, {6, 1.0 / 6}};
-    std::list<std::pair<double, double>> dice = {{-1, 1.0 / 2}, {1, 1.0 / 2}};
-
-    double expectedValue = ExpectedValue(dice);
-    cout << expectedValue << endl;
-
-    double variance = Variance(dice);
-    cout << variance << endl;
-
-    // auto function0 = Function<>::Constant(-11);
-    // auto function1 = Function<>::Exponent();
-    // auto function2 = Function<>::Power(1);
-    // auto function3 = Function<>::Composition(function1, function2);
-    // cout << function0.Parameter() << endl;
-    // cout << function1.Parameter() << endl;
-    // cout << function2.Parameter() << endl;
-    // cout << function3.Parameter() << endl;
+    std::vector<int> v = {1, 2, 3, 4, 5, 6};
+    DiscreteRandomVariable<int> d(v);
+    std::cout << d.ExpectedValue() << " " << d.Variance() << "\n";
 }
