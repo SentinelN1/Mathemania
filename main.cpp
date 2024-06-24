51eef824ae522db2d4e6f0a91d5d5e5a308e3ec4
Egor Antonov <egor.a.antonov@gmail.com>
2024-06-24 23:49:36 +0300

    Renamed files

--- main.cpp
+++ main.cpp
@@ -1,12 +1,23 @@
 #include <iostream>
-#include "Complex.hpp"
-#include "Function.hpp"
+#include "matrix.h"
 
-using namespace complex;
-using namespace function;
+// using namespace std;
+using namespace linal;
 
 int main()
 {
-    Function<Complex<double>> f = Function<Complex<double>>::Constant(Complex<double>(6, 4));
-    std::cout << f.Derivative().Parameter();
+    // Vector2D v = Vector2D(0.8, 0.6);
+    // Vector2D u = Vector2D(-0.8, -0.6);
+
+    // std::cout << Vector2D::Dot(v, u) << std::endl;
+    // std::cout << Vector2D::Rad(v, u) << std::endl;
+    // std::cout << Vector2D::Deg(v, u) << std::endl;
+
+    Vector x({1, 2, 3});
+    Vector y({2, 3, 1});
+    Vector z = x + y;
+
+    Real res = AngleRad(x, y) * 180.0 / M_PI;
+    std::cout << res;
+    return 0;
 }
